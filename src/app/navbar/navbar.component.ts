3748ab334b96be055bae75c58cd7f5af47a4e01d
bilol.kobilov <bilol.kobilov@s.amity.edu>
2023-04-16 01:22:03 +0500

    added animation to navbar options

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss'],
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-  Navbar= NavbarComponent;
+  currentRoute: string;
+  linkClicked: boolean = false;
+
+  constructor(private router: Router) {
+    this.currentRoute = this.router.url;
+    router.events.subscribe((val) => {
+      if (val instanceof NavigationEnd) {
+        this.currentRoute = val.url;
+      }
+    });
+  }
+
+  onClick(route: string) {
+    this.currentRoute = route;
+    this.linkClicked = true;
+  }
 }
+
